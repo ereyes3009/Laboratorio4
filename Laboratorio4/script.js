79cacf795597b757037430160e0e56d440653755
ereyes3009 <144548754+ereyes3009@users.noreply.github.com>
2023-09-24 23:09:22 -0500

    Add files via upload

---
+++ Laboratorio4/script.js
@@ -0,0 +1,35 @@
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
+
+function cargar(item){
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    mostrador.style.opacity = "1";
+    item.style.border = "2px solid red";
+    
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+
+    modeloSeleccionado.innerHTML = item.getElementsByTagName("p")[0].innerHTML;
+
+    descripSeleccionada.innerHTML = "Descripcion del modelo";
+
+    precioSeleccionado.innerHTML = item.getElementsByTagName("span")[0].innerHTML
+}
+function quitarBordes(){
+    var items = document.getElementsByClassName("item");
+    for(i = 0;i < items.length; i++){
+        items[i].style.border = "none";
+    }
+}
+
+function cerrar(){
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0";
+    seleccion.style.width = "0";
+    quitarBordes();
+}
